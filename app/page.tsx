c78074c328f16ab0c0dd6377f29f9a5f851b8d4b
Eda Akturk <edaakturk@Edas-MacBook-Pro.local>
2023-12-07 13:13:10 -0500

    Order based on dates

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,18 @@ import { allPosts } from "@/.contentlayer/generated"
 import Link from "next/link"
 
 export default function Home() {
+  // Sort posts by date, newest first
+  const sortedPosts = allPosts.sort((a, b) => {
+    // Convert dates to timestamps for comparison
+    const dateA = new Date(a.date).getTime();
+    const dateB = new Date(b.date).getTime();
+
+    return dateB - dateA; // For descending order (newest first)
+  });
+
   return (
     <div className="prose dark:prose-invert">
-      {allPosts.map((post) => (
+      {sortedPosts.map((post) => (
         <article key={post._id}>
           <Link href={post.slug}>
             <h2>{post.title}</h2>
